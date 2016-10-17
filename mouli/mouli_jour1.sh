ce26e479ba4ea882f1a73a492f7f7101c1388664
seif-eddine <sefboncoin@gmail.com>
2016-10-17 10:05:00 +0200

    mouli test

---
+++ mouli/mouli_jour1.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+## mouli_jour1.sh for  in /home/kirito/giy/serpentardEtna.git/trunk/mouli
+## 
+## Made by YOUSSEF seif-eddine
+## Login   <yousse_s@etna-alternance.net>
+## 
+## Started on  Mon Oct 17 10:04:33 2016 YOUSSEF seif-eddine
+## Last update Mon Oct 17 10:04:38 2016 YOUSSEF seif-eddine
+##
