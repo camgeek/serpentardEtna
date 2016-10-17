7752476c1f365a4411311d76eb7edaaedec5d2d3
seif-eddine <sefboncoin@gmail.com>
2016-10-17 19:28:10 +0200

    exe4

---
+++ mouli/main_next.c
@@ -0,0 +1,20 @@
+/*
+** main_next.c for  in /home/kirito/giy/serpentardEtna.git/trunk/mouli
+** 
+** Made by YOUSSEF seif-eddine
+** Login   <yousse_s@etna-alternance.net>
+** 
+** Started on  Mon Oct 17 19:02:05 2016 YOUSSEF seif-eddine
+** Last update Mon Oct 17 19:11:49 2016 YOUSSEF seif-eddine
+*/
+
+char	my_next_char(char c);
+void	my_putchar(char c);
+
+int	main()
+{
+  my_putchar(my_next_char('z'));
+  my_putchar(my_next_char('Z'));
+  my_putchar(my_next_char('5'));
+  my_putchar(my_next_char('f'));
+}
---
+++ mouli/main_prev.c
@@ -0,0 +1,20 @@
+/*
+** main_next.c for  in /home/kirito/giy/serpentardEtna.git/trunk/mouli
+** 
+** Made by YOUSSEF seif-eddine
+** Login   <yousse_s@etna-alternance.net>
+** 
+** Started on  Mon Oct 17 19:02:05 2016 YOUSSEF seif-eddine
+** Last update Mon Oct 17 19:22:54 2016 YOUSSEF seif-eddine
+*/
+
+char	my_prev_char(char c);
+void	my_putchar(char c);
+
+int	main()
+{
+  my_putchar(my_prev_char('A'));
+  my_putchar(my_prev_char('a'));
+  my_putchar(my_prev_char('5'));
+  my_putchar(my_prev_char('z'));
+}
